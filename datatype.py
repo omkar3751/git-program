b8e5b8f30851871df27c402eae8715aba03eafd9
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-21 01:58:48 -0700

    Create datatype.py

---
+++ datatype.py
@@ -0,0 +1,27 @@
+# Data Types
+1.string
+2.int
+3.float
+4.bool
+5.complex
+
+
+x = 'Hello'
+type(x)
+        :string
+
+x = 5
+type(x)
+        :int
+
+x = 3.5
+type(x)
+        :float
+
+x = True
+type(x)
+        :bool
+
+x = 3+5j
+type(x)
+        :complex
