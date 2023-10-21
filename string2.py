f2319c6ee96f6c1dee642a7a8851d1a38f112e5d
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-21 01:43:18 -0700

    string2.py

---
+++ string2.py
@@ -0,0 +1,16 @@
+# string in between character and step size of string
+# string in reverse order using slicing
+
+str='Hello India'
+
+#string slicing
+
+str[2:5:2]     # step size of 2
+Output:-
+        'lo'
+
+# reverse order
+
+str[::-1]      # reverse step size & -1
+Output:-
+        'aidnI olleH'
