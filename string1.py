33928dbcbfddfd1339ad07ee7e74bc3100d0445e
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-21 01:33:29 -0700

    string1.py

---
+++ string1.py
@@ -0,0 +1,20 @@
+# string slicing method 
+# string slicing using index numbers start the 0,1,2,...
+
+str='Hello India'
+
+str[o:5]
+Output:-
+        'Hello'
+
+str[:5]
+Output:-
+        'Hello'
+
+str[6:]
+Output:-
+        'India'
+
+str[:]
+Output:-
+        'Hello India'
