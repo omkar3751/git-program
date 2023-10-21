6f3a4a1c066f8abc3dec65fba022fab1dc3dc757
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-21 02:11:54 -0700

    arithmetic operator.py

---
+++ arithmetic operator.py
@@ -0,0 +1,18 @@
+# Arithmetic Operator
+
+1. Addition
+2. Substraction
+3. Multiplication
+4. Division
+5. Floor Division
+
+1. Addition :
+                  a + b
+2. Substraction :
+                  a - b
+3. Multiplication :
+                  a * b
+4. Division :
+                  a / b
+5. Floor Division :
+                  a // b
