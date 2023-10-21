9e575f8bc66b341ed0f1fba2bcb18d69cc19a724
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-21 01:24:10 -0700

    string.py

--- string.py
+++ string.py
@@ -1,11 +1,14 @@
 # string type example.
 # string denoted using a inverted comma(' ')
+# string size using len() function 
 
 x='Hello Everyone'
 print(x)
 print(type(x))
+print(len(x))
 
 Output :
         'Hello Everyone'
         < class 'str'>
+        14
 
