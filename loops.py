369a00d84bd81afcd06928034f54f9ff6d2cef71
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-22 06:32:09 -0700

    loops.py

---
+++ loops.py
@@ -0,0 +1,23 @@
+# loops execute for & while
+
+1.for loop
+
+for i in range(5):
+  print(i)
+Output:
+        0
+        1
+        3
+        4
+
+2. while loop
+
+while i <= 5:
+  print(i)
+  i = i + 1
+Output:
+        1
+        2
+        3
+        4
+        5
