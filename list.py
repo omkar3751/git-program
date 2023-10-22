d72ae779f4231585f5f2a0ffc8057c9ff1a729ca
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-22 02:58:59 -0700

    list.py

---
+++ list.py
@@ -0,0 +1,12 @@
+# list is data set any ime of data
+# list denoted a [] brackets
+
+lst=[1,2,3,4,5]
+print(lst)
+print(type(lst))
+print(len(lst))
+
+OUtput:
+        [1,2,3,4,5]
+        < class,'list'>
+        4
