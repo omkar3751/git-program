a55d032cb512b41b405313a23402e160737ef98b
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-22 03:33:00 -0700

    list2.py

---
+++ list2.py
@@ -0,0 +1,24 @@
+# occurence of element
+
+lst=[2,3,4,6,8,9,4,5,6,3,2,6,8,9,5,3]
+
+for i in lst:
+  print(f' occurence of {i} is {lst.count(i)}')
+
+Output:
+        occurence of 2 is 2
+        occurence of 3 is 3
+        occurence of 4 is 2
+        occurence of 6 is 3
+        occurence of 8 is 2
+        occurence of 9 is 2
+        occurence of 4 is 2
+        occurence of 5 is 2
+        occurence of 6 is 3
+        occurence of 3 is 3
+        occurence of 2 is 2
+        occurence of 6 is 3
+        occurence of 8 is 2
+        occurence of 9 is 2
+        occurence of 5 is 2
+        occurence of 3 is 3
