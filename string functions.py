5872f4d0c84cc7fa7609f2db6458a3d693612cb1
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-22 02:34:54 -0700

    string functions.py

---
+++ string functions.py
@@ -0,0 +1,28 @@
+# Striing Functions
+1.str.lower()       # lower case print the string
+2.str.upper()       # upper case print the string
+3.str.count()       # occurence of word in the string
+4.str.find()        # find the index particular element in string
+5.str.replace()     # to replace the word in the string
+
+str = "Hello World"
+1.
+  str.lower()
+Output:
+        'hello world'
+2.
+  str.upper()
+Output:
+        'HELLO WORLD'
+3.
+  str.count('o')                   # pass the argument
+OUtput:
+        2
+4.
+  str.find('W')                    # pass the argument
+Output:
+        6
+5.
+  str.replace("World","India")     # To pass the curret word & , replace word
+Output:
+        'Hello India'
