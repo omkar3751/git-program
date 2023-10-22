6cca9f980e1e6505f2f2525312eafcdf901d8c5b
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-22 03:13:21 -0700

    list1.py

---
+++ list1.py
@@ -0,0 +1,34 @@
+# Many type of data set
+lst=[51,"Omkar",5.16,True,3+5j]
+
+print(lst)
+print(len(lst))
+print(type(lst))
+Output:
+        [51,"Omkar",5.16,True,3+5j]
+        4
+        < class,'list'>
+
+print(lst[0])
+print(lst[1])
+print(lst[2])
+print(lst[3])
+print(lst[4])
+Output:
+        51
+        'Omkar'
+        5.16
+        True
+        3+5j
+
+print(type(lst[0]))
+print(type(lst[1]))
+print(type(lst[2]))
+print(type(lst[3]))
+print(type(lst[4]))
+Output:
+        < class,'int'>
+        < class,'str'>
+        < class,'float'>
+        < class,'bool'>
+        < class,'complex'>
