16b790bdee8150cac74aa5eaa6b830536a1bc607
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-22 04:16:23 -0700

    addition.py

---
+++ addition.py
@@ -0,0 +1,23 @@
+# 1. addition of two numbers already define
+
+a = 10
+b = 20
+
+c = a + b
+ print(c)
+Output:
+        30
+
+# 2. addition of two numbers user input values
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+c = a + b
+
+print(c)
+Output:
+        a = 30
+        b = 20
+
+        50
