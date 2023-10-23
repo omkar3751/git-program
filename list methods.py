7164e561aa1234546f02ab1e14ce245bb9fc3ee0
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-23 03:38:32 -0700

    list methods.py

--- list methods.py
+++ list methods.py
@@ -4,6 +4,9 @@
 2.list.extend()          # input the multiple value in last index
 3.list.pop()             # onetime show and permanantly delete the value
 4.list.sort()            # sort by ascending order
+5.list.remove()          # remove the particular value
+6.list.count()           # number of time thats occurence 
+7.list.index()           # find the index number
 
 lst=['python','html','css']                                
 
@@ -19,6 +22,19 @@ lst.pop(1)                                                # pass the index numbe
 Output:
         ['python','css','php','xml','javascript']
 
-lst.sort()                                                 # defaultly ascending order
+lst.sort()                                                # defaultly ascending order
 Output:
         ['css','javascript','php','python','xml']
+
+lst.remove('xml')                                          # pass the argument
+Output:
+        ['css','javascript','php','python']
+
+lst=[1,2,3,4,5,6,,2,7,8,2,9]
+lst.count(2)                                               # pass the argument
+Output:
+        3
+
+lst.index('css')                                           # pass the argument
+Output:
+        2
