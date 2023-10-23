9d3340570bcc0ed103ee3770f8deac9fac782a4e
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-23 03:13:10 -0700

    list6.py

---
+++ list6.py
@@ -0,0 +1,15 @@
+# enumerate function it returns the index & value print
+
+lst=["apple","cherry","banana","grapes","mango","orange"]
+
+for i,j in enumerate(lst):
+    print(i,j)                        # only print(i) return the index
+                                      # only print(j) return the value
+
+Output:
+        0  'apple
+        1  'cherry'
+        2  'banana'
+        3  'grapes'
+        4  'mango'
+        5  'orange'
