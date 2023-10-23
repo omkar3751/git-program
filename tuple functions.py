c1555ab6579ce9cd289775cb1ed38e0fbccb1d03
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-23 07:52:09 -0700

    tuple functions.py

---
+++ tuple functions.py
@@ -0,0 +1,26 @@
+# tuple is immutable
+# In this functions pass the tuple name
+
+1.min()              # minimum value in tpl
+2.max()              # maximum value in tpl
+3.len()              # length of tpl
+4.sorted()           # sort in ascending order 
+
+tpl=(1,3,5,2,4)
+
+min(tpl)
+Output:
+        1
+
+max(tpl)
+Output:
+        5
+
+len(tpl)
+Output:
+        11
+
+sorted(tpl)
+Output:
+        (1,2,3,4,5)
+        
