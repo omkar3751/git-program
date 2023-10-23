ffce969bc8cda944be4550dd76b71707944357ed
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-23 04:48:06 -0700

    comparison operator.py

---
+++ comparison operator.py
@@ -0,0 +1,29 @@
+# comparison operators:
+1. <
+2. >
+3. <=
+4. >=
+5. ==
+6. !=
+
+a=10
+b=20
+
+a<b
+Output:
+        True
+a>b
+Output:
+        False
+a<=b
+Output:
+        True
+a>=b
+Output:
+        False
+a==b
+Output:
+        False
+a!=b
+Output:
+        True
