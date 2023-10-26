f41d71de578bf151023d5703159c3a0928860cc0
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-26 09:48:33 -0700

    Create dict1.py

---
+++ dict1.py
@@ -0,0 +1,19 @@
+# functions & methods
+
+dic = {'Name':['sagar','akshay','kumar'],'age':{1:21,2:23,3:24}}
+print(dic)
+Output:
+        {'Name': ['sagar', 'akshay', 'kumar'],
+         'age': {1: 21, 2: 23, 3: 24}}
+
+dic.update({emp_no:[101,102,103]}
+print(dic)
+Output:
+        {'Name': ['sagar', 'akshay'],
+         'age': {1: 21, 2: 23, 3: 24}
+         'emp_no': {101,102,103}}
+
+dic.pop('age')
+Output:
+        age: {1: 21, 2:23, 3:24}
+
