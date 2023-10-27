13586cb9c62f24df087ad42c1cf8b85d41a26e56
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-27 07:45:26 -0700

    condition.py

---
+++ condition.py
@@ -0,0 +1,21 @@
+# if , elif , else conditions applying to the programmes.
+
+age = int(input("Enter the Age:"))
+
+if age > 18:
+  print("You are Adult")
+elif age > 6:
+  print("You are Student")
+else:
+  print("You are Child)
+
+
+Output:
+        Enter the Age: 22
+        'You are Adult
+
+        Enter the Age: 8
+        'You are Student
+
+        Enter the Age: 4
+        'You are Child
