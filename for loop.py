d55c13b8f51294b145f5fcd0be2a3f4ef14218ee
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-27 07:55:51 -0700

    for loop.py

---
+++ for loop.py
@@ -0,0 +1,31 @@
+# to execute for loop with range Function
+
+for i in range(1,11):
+  print(i)
+
+Output:
+        1
+        2
+        3
+        4
+        5
+        6
+        7
+        8
+        9
+        10
+
+for i in range(1,11):
+  print(i**2)
+  Output:
+          1
+          4
+          9
+          16
+          25
+          36
+          49
+          64
+          81
+          100
+  
