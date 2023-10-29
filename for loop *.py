fb31df55c82bef2925f62d9c997c15034825cc89
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-29 01:56:28 -0700

    Create for loop *.py
    star pattern

---
+++ for loop *.py
@@ -0,0 +1,20 @@
+# print the * pattern 
+1.
+for i in range(1,5+1):
+    print(i * "*")
+Output:
+        *
+        **
+        ***
+        ****
+        *****
+          
+2.
+for i in range(5,0,-1):
+    print(i * "*")
+Output:
+        *****
+        ****
+        ***
+        **
+        *
