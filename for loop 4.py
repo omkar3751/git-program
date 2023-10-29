7821682e2db6688d3cf34c23d93c2231945b4b40
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-28 23:51:22 -0700

    for loop 4.py

---
+++ for loop 4.py
@@ -0,0 +1,12 @@
+# Find the even Or odd numbers
+num=[1,2,3,4,5,6,7,8,9] 
+
+for i in num:
+    if i % 2 ==0:
+        print(f'Even Number are:{i}')
+    else:
+        print(f'Odd number are: {i}')
+
+Output:
+        Even Number are : 2,4,6,8
+        Odd Number are  : 1,3,5,7,9
