54309311c8c3657bfbdc4c80020fc1c5a8a1f979
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-28 23:48:22 -0700

    for loop 3 .py

---
+++ for loop 3 .py
@@ -0,0 +1,19 @@
+#create table any numbers with for loop
+
+num=int(input("Enter Value:  "))           
+for i in range(1,11):
+    print(f'{num} * {i} = {num * i}')
+
+Output:
+        Enter Value:  222
+
+                      222 * 1 = 222
+                      222 * 2 = 444
+                      222 * 3 = 666
+                      222 * 4 = 888
+                      222 * 5 = 1110
+                      222 * 6 = 1332
+                      222 * 7 = 1554
+                      222 * 8 = 1776
+                      222 * 9 = 1998
+                      222 * 10 = 2220
