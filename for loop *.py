a8ea6f812ac6b4457a4405be69b7a77eb7be094e
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-29 02:49:24 -0700

    for loop *.py

--- for loop *.py
+++ for loop *.py
@@ -5,18 +5,44 @@ for i in range(1,5+1):
 
 Output:
         *
-        **
-        ***
-        ****
-        *****
+        * *
+        * * *
+        * * * *
+        * * * * *
           
 2.
 for i in range(5,0,-1):
     print(i * "*")
 
 Output:
-        *****
-        ****
-        ***
-        **
+        * * * * *
+        * * * *
+        * * *
+        * *
         *
+3.
+n=5
+for i in range(n):
+    for j in range(i+1):
+        print(' ',end=' ')
+    for k in range(i,n):
+        print('*',end=' ')
+    print()
+Output:
+         * * * * * 
+           * * * * 
+             * * * 
+               * * 
+                 * 
+4.
+n=5
+for i in range(n):
+    for j in range(n):
+        print('*',end=' ')
+    print()
+Output:
+        *  *  *  *  * 
+        *  *  *  *  * 
+        *  *  *  *  * 
+        *  *  *  *  * 
+        *  *  *  *  * 
