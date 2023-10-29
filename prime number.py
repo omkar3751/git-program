f1f3047c7b83560009fe4aac95c175aa8695073d
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-29 01:42:37 -0700

    prime number.py

--- prime number.py
+++ prime number.py
@@ -1,4 +1,4 @@
-# Print the prime numbers
+# 1. Print the prime numbers
 
 lower_value=int(input("Enter the lower limit: "))
 upper_value=int(input("Enter the upper limit: "))
@@ -20,3 +20,27 @@ Output:
         13
         17
         19
+# 2. number of element in the list
+
+num=int(input("Enter the number of element: "))
+
+lst=[]
+prime=[]
+
+for i in range(1,num+1):
+    val=int(input("Enter value: "))                         
+    lst.append(val)
+    if val > 1:
+        for i in range(2,val):
+            if val % i ==0:
+                break
+        else:
+            prime.append(val)
+print(prime)
+
+Output:
+        Enter the number of element: 3
+        Enter value: 4
+        Enter value: 7
+        Enter value: 11
+        [7, 11]
