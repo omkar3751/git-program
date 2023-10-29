984323bce70ca22b294259b6dd99ea4ec602d738
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-28 23:58:45 -0700

    while loop 1.py

---
+++ while loop 1.py
@@ -0,0 +1,19 @@
+# Take an example of while 
+
+i = 1
+
+while i<11:             # pass the condition
+ print(i)
+ i+=1                   # increment the value
+
+Output :
+          1
+          2
+          3
+          4
+          5
+          6
+          7
+          8
+          9
+          10
