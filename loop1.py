586905ae6bfd5ad20bd6b8694b8f1d5803d84ee1
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-28 23:39:50 -0700

    loop1.py

---
+++ loop1.py
@@ -0,0 +1,14 @@
+# Enumaeration of the elements in the list
+
+languages = ['Python', 'Java', 'JavaScript', 'C', 'C++', 'PHP']
+
+for i,j in enumerate(languages):
+ print(i,j)
+
+Output:
+        0  Python
+        1  Java
+        2  JavaScript
+        3  C
+        4  C++
+        5  PHP
