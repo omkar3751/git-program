d8358d0ac8157f0286548392924e3ce46df5aae0
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-30 06:49:59 -0700

    def functions.py

---
+++ def functions.py
@@ -0,0 +1,15 @@
+# def function used in mathematical operations.
+
+def math(x,y):
+  z = x + y
+  z1 = x - y
+  z2 = x * y
+  z3 = x / y
+  z4 = x % y
+  z5 = x//y
+  return z,z1,z2,z3,z4,z5
+
+math(5,3)
+
+Output:
+        (8, 2, 15, 1.6666666666666667, 2, 1)
