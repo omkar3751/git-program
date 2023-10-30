0f448fcb6551d97deda3ffd42efadbb7c1f6ef5f
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-10-30 07:03:12 -0700

    def functions 1.py

---
+++ def functions 1.py
@@ -0,0 +1,16 @@
+# def functions
+
+def multiply(a,b,c):
+    """This function prints the product of three numbers"""
+    print("Machine Learning")
+    x = a * b * c
+    print(f'product of three numbers are {x}')
+    t = a * c
+    print(f'product of first and third number is {t}')
+Output:
+
+      multiply(2,3,4)
+
+      Machine Learning
+      product of three numbers are 24
+      product of first and third number is 8
