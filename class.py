86c04e41e7374f8dfe6302ddf0d06bf5f61b077d
omkar3751 <146797555+omkar3751@users.noreply.github.com>
2023-11-07 21:54:07 -0800

    class.py

---
+++ class.py
@@ -0,0 +1,14 @@
+# simple example
+
+class Car:
+  model = 'Volvo'
+  color = 'Blue'
+  
+car = Car()
+
+print(car.model)                # calling method
+print(car.color)        
+
+Output :
+        Volvo
+        Blue
